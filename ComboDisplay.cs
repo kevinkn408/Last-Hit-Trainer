22a82a60613ebbdfa911eb38a160d5e65c7a68f2
Kevin Nguyen <kevinkn408@gmail.com>
2021-03-23 13:40:37 -0700

    Last Hit Trainer Latest Commit

---
+++ ComboDisplay.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace RPG.Stats
+{
+    public class ComboDisplay : MonoBehaviour
+    {
+        [SerializeField] Text comboDisplay = null;
+        [SerializeField] TextMeshProUGUI comboDisplayTMP = null;
+        LastHitManager lastHitManager;
+
+        void Awake()
+        {
+            lastHitManager = FindObjectOfType<LastHitManager>();
+        }
+
+        void Update()
+        {
+            if (comboDisplayTMP != null)
+            {
+                UI_DisplayCombo();
+            }
+            else
+            {
+                comboDisplay.text = lastHitManager.score.ToString();
+            }
+        }
+
+        private void UI_DisplayCombo()
+        {
+            comboDisplayTMP.text = lastHitManager.score.ToString();
+            if (lastHitManager.score > 1)
+            {
+                comboDisplayTMP.enabled = true;
+            }
+            else
+            {
+                comboDisplayTMP.enabled = false;
+            }
+        }
+    }
+}
---
+++ GoldDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+
+namespace RPG.Stats
+{
+    public class GoldDisplay : MonoBehaviour
+    {
+        [SerializeField] Text goldDisplay = null;
+        [SerializeField] TextMeshProUGUI goldDisplayTMP = null;
+        LastHitManager lastHitManager;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            lastHitManager = FindObjectOfType<LastHitManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (goldDisplayTMP != null)
+            {
+                goldDisplayTMP.text = lastHitManager.currency.ToString();
+            }
+            else
+            {
+                goldDisplay.text = lastHitManager.currency.ToString();
+            }
+        }
+    }
+}
